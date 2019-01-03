892625d7ac9e1d1d7931b9952bbca84c3a991d30
greg_toombs <greg.toombs@oracle.com>
2019-01-03 15:05:46 -0500

    Line up

--- error.h
+++ error.h
@@ -2,9 +2,9 @@
 
 #include <stdbool.h>
 
-bool check_c(int result, const char *action);
-void assert_c(int result, const char *action);
-bool check_b(bool result, const char *action);
+bool  check_c( int result, const char *action);
+void assert_c( int result, const char *action);
+bool  check_b(bool result, const char *action);
 void assert_b(bool result, const char *action);
-bool check_p(void *p, const char *action);
-void assert_p(void *p, const char *action);
+bool  check_p(    void *p, const char *action);
+void assert_p(    void *p, const char *action);
