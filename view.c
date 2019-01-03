edcf1b8c8f30bcbafeb79eea646add8bf41fdbce
greg_toombs <greg.toombs@oracle.com>
2019-01-03 15:03:57 -0500

    Initialize palette

--- view.c
+++ view.c
@@ -12,6 +12,25 @@ struct {
     WINDOW *win;
 } static view;
 
+static void init_palette() {
+    const unsigned pm = 1000, // Resolution of ncurses colours
+                   rm = 6,    // Red channel resolution
+                   gm = 7,    // Green channel resolution
+                   bm = 6,    // Blue channel resolution
+                   palsize = rm*gm*bm; // Palette colours
+
+    // todo - make these realistic
+    // assert_b(COLOR_PAIRS >= 16, "support enough colour pairs");
+    assert_b(COLORS >= palsize, "support enough colours");
+
+    for (unsigned p = 0; p < palsize; p++) {
+        unsigned r = pm * ( p            % rm) / (rm-1),
+                 g = pm * ((p /  rm    ) % gm) / (gm-1),
+                 b = pm * ((p / (rm*gm)) % bm) / (bm-1);
+        assert_b(ERR != init_color(p, r, g, b), "add colour index");
+    }
+}
+
 void view_init() {
     // Set entire locale based on environment
     // Recommended by man ncurses(3X)
@@ -27,10 +46,7 @@ void view_init() {
     assert_b(ERR != curs_set(INVIS_CUR), "hide the cursor");
     assert_b(ERR != keypad(view.win, true), "enable translation of arrow keys");
 
-    // todo - make these realistic
-    assert_b(COLOR_PAIRS >= 16, "support enough colour pairs");
-    assert_b(COLORS >= 256, "support enough colours");
-    // note - each colour channel supports 1000 values
+    init_palette();
 }
 
 void view_destroy() {
