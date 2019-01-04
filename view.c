3ab2ea3217b78f0f28f821f7b7d977b909caf6db
greg_toombs <greg.toombs@oracle.com>
2019-01-04 18:35:30 -0500

    Compiles but fails on pair init

--- view.c
+++ view.c
@@ -13,13 +13,12 @@ struct {
 } static view;
 
 static void init_palette() {
-    const unsigned pm = 1000, // Resolution of ncurses colours
-                   palsize = RRES*GRES*BRES; // Palette colours
+    const unsigned pm = 1000; // Resolution of ncurses colours
 
     assert_b(COLOR_PAIRS >= 0x10000, "support enough colour pairs");
-    assert_b(COLORS >= palsize, "support enough colours");
+    assert_b(COLORS >= PALSIZE, "support enough colours");
 
-    for (unsigned p = 0; p < palsize; p++) {
+    for (unsigned p = 0; p < PALSIZE; p++) {
         unsigned r = pm * ( p                % RRES) / (RRES-1),
                  g = pm * ((p /  RRES      ) % GRES) / (GRES-1),
                  b = pm * ((p / (RRES*GRES)) % BRES) / (BRES-1);
@@ -54,6 +53,8 @@ static void wave() {
     // Draw an animated wave emanating from the center outwards, and filling
     // from black to a grey pattern
 
+    const int off = 2; // Offset between fore and back
+    // Black through blue through white
     const NCURSES_COLOR_T scale[] = {
         C2P(0,0,0),
         C2P(0,0,1),
@@ -67,23 +68,32 @@ static void wave() {
         C2P(4,6,5),
         C2P(5,6,5)
     };
+    // Number of pairs
+    const int n = sizeof(scale)/sizeof(*scale) - off;
+
+    // Set up pairs
+    NCURSES_COLOR_T pairs[n];
+    for (int i = 0; i < n; i++) {
+        NCURSES_COLOR_T b = scale[i],
+                        f = scale[i+off],
+                        p = i+1; // FBPAIR(f, b);
+        pairs[i] = p;
+        char action[64];
+        snprintf(action, sizeof(action),
+                 "init scale pair f=%d b=%d p=0x%04hX",
+                 f, b, p);
+        assert_n(init_pair(p, f, b), action);
+    }
+
+    // Test output
+    assert_n(wmove(view.win, 0, 0), "move cur");
+    for (int i = 0; i < n; i++) {
+        assert_n(wattron(view.win, COLOR_PAIR(pairs[i])), "switch colour");
+        assert_n(waddch(view.win, 'X'), "output char");
+    }
 
     // write a char to the screen with mvwaddch
     // (int) mvwaddch (WINDOW *, int, int, const chtype);	/* generated */
-
-    // attron(COLOR_PAIR(BG_COLOR));
-    // int wattron(WINDOW *win, int attrs);
-
-    // * These apply to the first 256 color pairs.
-    // #define COLOR_PAIR(n)	((n << 8) & 0xFF00)
-
-    // #define A_COLOR	     	0xFF00
-    // #define NCURSES_BITS(mask,shift) (mask << ((shift) + 8))
-    // #define NCURSES_CAST(type,value) (type)(value)
-    // typedef unsigned chtype;
-    // #define NCURSES_ATTR_SHIFT       8
-
-
 }
 
 void view_splash() {
--- view.h
+++ view.h
@@ -4,12 +4,16 @@
 #define RRES 6
 #define GRES 7
 #define BRES 6
+#define PALSIZE (RRES*GRES*BRES)
 
 // Colour components to palette index
 // 0 <= R < RRES
 // 0 <= G < GRES
 // 0 <= B < BRES
-#define C2P(r, g, b) (r + g*RRES + b*RRES*GRES)
+#define C2P(r, g, b) (r + (g)*RRES + (b)*RRES*GRES)
+
+// Form a pair ID from back and fore palette indices
+#define FBPAIR(f, b) (1 + f + PALSIZE*(b))
 
 void view_init();
 void view_destroy();
