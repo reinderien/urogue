09552435292b97bb892b28799dce57a5ef95a889
greg_toombs <greg.toombs@oracle.com>
2019-01-11 20:37:22 -0500

    Follow ANSI 256 palette default

--- palette/palette.py
+++ palette/palette.py
@@ -5,10 +5,13 @@ from PIL import Image
 def pal_colours(*args, pm):
     rm, gm, bm = args
 
+    # For ANSI compatibility:
+    # From fastest to slowest axis: B, R, G
+
     for p in range(rm*gm*bm):
-        ri =  p             % rm
-        gi = (p //  rm    ) % gm
-        bi = (p // (rm*gm)) % bm
+        bi =  p             % bm
+        ri = (p //  bm    ) % rm
+        gi = (p // (bm*rm)) % gm
         r = pm * ri // (rm-1)
         g = pm * gi // (gm-1)
         b = pm * bi // (bm-1)
@@ -16,10 +19,10 @@ def pal_colours(*args, pm):
 
 def pal_image(*args):
     rm, gm, bm = args
-    im = Image.new('RGB', (gm, (rm+1)*bm))
+    im = Image.new('RGB', ((rm+1)*gm, bm))
     for ri, gi, bi, r, g, b in pal_colours(*args, pm=255):
-        x = gi
-        y = ri + (1+rm)*bi
+        y = ri
+        x = bi + (1+bm)*gi
         im.putpixel((x, y), (r, g, b))
     return im
 
@@ -29,9 +32,9 @@ def pal_dump(*args):
     for p, colours in zip(range(rm*gm*bm), pal_colours(*args, pm=pm)):
         r, g, b = colours[3:]
         pt = (
-             r    * (rm-1)         +
-            (g+1) * (gm-1) * rm    +
-             b    * (bm-1) * rm*gm
+             b    * (bm-1)         +
+            (r+1) * (rm-1) * bm    +
+             g    * (gm-1) * bm*rm
         ) // pm
 
         assert(p == pt)
@@ -39,8 +42,11 @@ def pal_dump(*args):
         print('%3d %4d %4d %4d' % (p, r, g, b))
 
 # Alternatives:
-# 6, 6, 6 - perfect uniformity but poor usage
 # 8, 8, 4 - perfect usage but poor uniformity
-test_colours = 6, 7, 6
+# 6, 7, 6 - good balance of uniformity and usage
+
+# 6, 6, 6 - perfect uniformity but poor usage
+# Used by https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
+test_colours = 6, 6, 6
 pal_image(*test_colours).save('palette.png')
 pal_dump(*test_colours)
